c3a2afbd83a7447561384ba7d34e977b0cd067cc
ngangavic <ngangavictor10@gmail.com>
2019-10-08 13:18:20 +0300

    custom button

--- app/src/main/java/com/example/twitterlogin/MainActivity.kt
+++ app/src/main/java/com/example/twitterlogin/MainActivity.kt
@@ -2,17 +2,22 @@ package com.example.twitterlogin
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.twitter.sdk.android.core.*
 import com.twitter.sdk.android.core.TwitterCore
+import com.twitter.sdk.android.core.identity.TwitterAuthClient
 import com.twitter.sdk.android.core.identity.TwitterLoginButton
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btnLogin:TwitterLoginButton
+    lateinit var btnLogin: TwitterLoginButton
+    lateinit var button: Button
+
+    lateinit var mTwitterAuthClient: TwitterAuthClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,18 +30,48 @@ class MainActivity : AppCompatActivity() {
         Twitter.initialize(this)
         setContentView(R.layout.activity_main)
 
-        btnLogin=findViewById(R.id.login_button)
+        button = findViewById(R.id.button)
+        mTwitterAuthClient = TwitterAuthClient()
+
+
+//        val callback=object :Callback<TwitterSession>(){
+//            override fun success(result: Result<TwitterSession>?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//            override fun failure(exception: TwitterException?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//        }
+        button.setOnClickListener {
+            mTwitterAuthClient.authorize(
+                this,
+                object : com.twitter.sdk.android.core.Callback<TwitterSession>() {
+
+                    override fun success(twitterSessionResult: Result<TwitterSession>) {
+                        // Success
+                    }
+
+                    override fun failure(e: TwitterException) {
+                        e.printStackTrace()
+                    }
+                })
+        }
+
+        btnLogin = findViewById(R.id.login_button)
         btnLogin.setCallback(object : Callback<TwitterSession>() {
             override fun success(result: Result<TwitterSession>) {
                 val session = TwitterCore.getInstance().sessionManager.activeSession
                 val authToken = session.authToken
-                Toast.makeText(applicationContext,"Auth token: "+authToken,Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Auth token: " + authToken, Toast.LENGTH_SHORT)
+                    .show()
 
                 loginMethod(session)
             }
 
             override fun failure(exception: TwitterException) {
-                Toast.makeText(getApplicationContext(),"Login fail", Toast.LENGTH_LONG).show()
+                Toast.makeText(getApplicationContext(), "Login fail", Toast.LENGTH_LONG).show()
                 exception.printStackTrace()
             }
         })
@@ -46,7 +81,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun loginMethod(session: TwitterSession?) {
         val userName = session?.userName
-        Toast.makeText(this,"User name is "+userName,Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "User name is " + userName, Toast.LENGTH_LONG).show()
 
     }
 
@@ -55,6 +90,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         // Pass the activity result to the login button.
-        btnLogin.onActivityResult(requestCode, resultCode, data);
+        btnLogin.onActivityResult(requestCode, resultCode, data)
+        mTwitterAuthClient.onActivityResult(requestCode, resultCode, data)
     }
 }
