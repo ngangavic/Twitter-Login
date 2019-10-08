07ede3b8c32b49c7e3f78c8225090aacc33768f7
ngangavic <ngangavictor10@gmail.com>
2019-10-08 11:52:32 +0300

    twitter api

--- app/src/main/java/com/example/twitterlogin/MainActivity.kt
+++ app/src/main/java/com/example/twitterlogin/MainActivity.kt
@@ -1,31 +1,60 @@
 package com.example.twitterlogin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.twitter.sdk.android.core.Twitter
-import com.twitter.sdk.android.core.TwitterAuthConfig
-import com.twitter.sdk.android.core.DefaultLogger
-import com.twitter.sdk.android.core.TwitterConfig
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.twitter.sdk.android.core.*
+import com.twitter.sdk.android.core.TwitterCore
+import com.twitter.sdk.android.core.identity.TwitterLoginButton
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var btnLogin:TwitterLoginButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Twitter.initialize(this)
 
         val config = TwitterConfig.Builder(this)
             .logger(DefaultLogger(Log.DEBUG))
-            .twitterAuthConfig(TwitterAuthConfig("QofiFEEDdUVPtAmpTTc0AyAw1", "q6fKdh2wZlVaSYrXBwRCnD8MJpom6ZOt7r67otfsafW7EVZqx8"))
+            .twitterAuthConfig(TwitterAuthConfig("", ""))
             .debug(true)
             .build()
         Twitter.initialize(config)
+        setContentView(R.layout.activity_main)
+
+        btnLogin=findViewById(R.id.login_button)
+        btnLogin.setCallback(object : Callback<TwitterSession>() {
+            override fun success(result: Result<TwitterSession>) {
+                val session = TwitterCore.getInstance().sessionManager.activeSession
+                val authToken = session.authToken
+                Toast.makeText(applicationContext,"Auth token: "+authToken,Toast.LENGTH_SHORT).show()
+
+                loginMethod(session)
+            }
+
+            override fun failure(exception: TwitterException) {
+                Toast.makeText(getApplicationContext(),"Login fail", Toast.LENGTH_LONG).show()
+                exception.printStackTrace()
+            }
+        })
 
 
+    }
+
+    private fun loginMethod(session: TwitterSession?) {
+        val userName = session?.userName
+        Toast.makeText(this,"User name is "+userName,Toast.LENGTH_LONG).show()
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result to the login button.
+        btnLogin.onActivityResult(requestCode, resultCode, data);
     }
 }
